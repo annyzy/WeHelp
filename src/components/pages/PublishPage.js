1ec9baee1faaf45c9980ca7d4ab58f6ee74e2b5c
Jiajian Lin <jaxlin@g.ucla.edu>
2020-10-27 02:37:13 -0700

    add textinput for publish page

--- src/components/pages/PublishPage.js
+++ src/components/pages/PublishPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated, View, Text, StyleSheet, Button } from 'react-native';
+import { Animated, View, Text, StyleSheet, Button, TextInput } from 'react-native';
 import { PageHeader } from './PageHeader'
 
 export function PublishPage({navigation}) {
@@ -7,11 +7,18 @@ export function PublishPage({navigation}) {
         <View style={{flex: 1, justifyContent: "space-between"}}>
             <View>
                 <PageHeader pageName="Publish" />
+                <View style = {{ padding: 15}}>
+                <TextInput clearButtonMode="always"
+                    multiline={true}
+                    numberOfLines={20}
+                    style={{fontSize:20}}
+                    autoFocus={true}
+                    // value={"PlaceHolder"}
+                />
             </View>
-            <View style={{alignItems: "center"}}>
-                <Text>text!</Text>
             </View>
         <Button title="Go back" onPress={() => navigation.goBack()} />
+        <View></View>
         </View>
     );
   }
