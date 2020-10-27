cd671a396fab43b535d5cfd7a5288b311092de3e
Jiajian Lin <jaxlin@g.ucla.edu>
2020-10-27 01:54:31 -0700

    add modal publish page

--- src/components/pages/PageNavigation.js
+++ src/components/pages/PageNavigation.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
+import { View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { HomePage } from './HomePage'
 import { TaskPage } from './TaskPage'
 import { PublishPage } from './PublishPage'
@@ -8,19 +10,41 @@ import { MessagePage } from './MessagePage'
 import { UserPage } from './UserPage'
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export class PageNavigation extends Component {
     render() {
       return (
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomePage} />
-            <Tab.Screen name="Task" component={TaskPage} />
-            <Tab.Screen name="Publish" component={PublishPage} />
-            <Tab.Screen name="Message" component={MessagePage} />
-            <Tab.Screen name="User" component={UserPage} />
-          </Tab.Navigator>
+          <Stack.Navigator headerMode="false" mode="modal"> 
+            <Stack.Screen name="Main" component={TabNavigator}/>
+            <Stack.Screen name="Publish" component={PublishPage}/>
+          </Stack.Navigator>
         </NavigationContainer>
      );
     }
+}
+
+function TabNavigator({ navigation }) {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={HomePage} />
+      <Tab.Screen name="Task" component={TaskPage} />
+      <Tab.Screen name="Publish" component={ModalPlaceHolder} 
+      listeners={() => ({
+        tabPress: e => {
+          e.preventDefault();
+          navigation.navigate("Publish");
+        },
+      })}/>
+      <Tab.Screen name="Message" component={MessagePage} />
+      <Tab.Screen name="User" component={UserPage} />
+    </Tab.Navigator>
+  );
+}
+
+function ModalPlaceHolder() {
+  return (
+    <View/> 
+  );
 }
--- src/components/pages/PublishPage.js
+++ src/components/pages/PublishPage.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Animated, View, Text, StyleSheet, Button } from 'react-native';
 import { PageHeader } from './PageHeader'
 
-export function PublishPage() {
+export function PublishPage({navigation}) {
     return (
         <View style={{flex: 1, justifyContent: "space-between"}}>
-        <View>
-            <PageHeader pageName="Publish" />
-        </View>
-        <View style={{alignItems: "center"}}>
-            <Text>text!</Text>
-        </View>
-        <View></View>
+            <View>
+                <PageHeader pageName="Publish" />
+            </View>
+            <View style={{alignItems: "center"}}>
+                <Text>text!</Text>
+            </View>
+        <Button title="Go back" onPress={() => navigation.goBack()} />
         </View>
     );
   }
