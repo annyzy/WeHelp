73447a7c2d89db35325146c243cb71d5ddf9e468
dliang-lbl <yao090222@ucla.edu>
2020-10-30 17:06:46 -0700

    add requirement.txt and wsgi.py

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -13,4 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../WeHelpServer'))
+
+from apps import WeHelpServer as application
+
+
