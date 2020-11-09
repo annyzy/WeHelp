af25b35efa506a1ca9c57bf4ebe5ee667651fc0a
dliang-lbl <yao090222@ucla.edu>
2020-11-08 22:54:37 -0800

    server: char maxlength

--- server/WeHelpServer/models.py
+++ server/WeHelpServer/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(models.Model):
     coins = models.IntegerField()
-    icon = models.CharField()
+    icon = models.CharField(max_length=4096)
     rating = models.FloatField()
     create_date = models.DateTimeField()
     email = models.EmailField()
