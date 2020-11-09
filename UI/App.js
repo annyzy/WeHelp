eab4e36a0afd0e48494155736b954ca7640fa440
Jiajian Lin <jaxlin@g.ucla.edu>
2020-11-09 15:13:39 -0800

    UI: add UserContext.js

--- UI/App.js
+++ UI/App.js
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react';
 import { Platform } from 'react-native';
 import { PageNavigation } from './src/components/PageNavigation';
 import { LoginPage } from './src/pages/LoginPage';
-import { EventRegister } from 'react-native-event-listeners';
+import { UserContext } from './src/components/UserContext';
 
 export default function App() {
   const [user, setUser] = useState({
@@ -22,25 +22,18 @@ export default function App() {
   }, []);
 
   if (Platform.OS == 'web') {
-    return <PageNavigation/>;
+    return (
+    <UserContext.Provider value={user}>
+      <PageNavigation/>
+    </UserContext.Provider>
+    );
   }
   else if (user.signedIn) {
-    //fetch('http://34.94.101.183:80/WeHelp/', 
-    //{
-    //  method:'POST',
-    //  body: JSON.stringify({
-    //    func:'signIn', email:user.email, icon:user.photoUrl
-    //  })})
-    //  .then(async (resp)=>{
-    //    let found = await resp.json();
-    //    alert('hello ' + user.name + ' UID:' + found['UID'] + ' photoUrl:' + found['icon']);
-    //    //setUser({photoUrl: found['icon'], name: user.name, signedIn: user.signedIn, email: user.email});
-    //    //changeUser(user.name, found['icon'], user.email);
-    //  }).catch(() => {
-    //    alert('Fetch failed');
-    //});
-    //EventRegister.emit('iconChanged', user.photoUrl);
-    return <PageNavigation/>;
+    return (
+      <UserContext.Provider value={user}>
+        <PageNavigation/>
+      </UserContext.Provider>
+      );  
   }
   else {
     return <LoginPage changeUser={changeUser} />;
--- UI/src/pages/UserPage.js
+++ UI/src/pages/UserPage.js
@@ -1,35 +1,21 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { View, Text, Image } from 'react-native';
 import { PageHeader } from '../components/PageHeader';
-import { EventRegister } from 'react-native-event-listeners';
+import { UserContext } from '../components/UserContext';
 
-export class UserPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            icon: "no icon"
-        }
-    }
-    
-    //changeIcon() {
-        //this.listener = EventRegister.addEventListener('iconChanged', (newIcon) => {
-            //this.setState({
-                //icon: newIcon,
-            //})
-        //})
-    //}
-    render() {
-        return (
-            <View style={{flex: 1, justifyContent: 'space-between', backgroundColor: 'white'}}>
-                <PageHeader centerComp={<Text>User</Text>} />
-                <Image source={{
-                    uri:'https://i.pinimg.com/originals/0c/1c/a1/0c1ca1955e2b0c5469ba17da2b1b9b96.jpg'
-                }}/>
-                <View style={{alignItems: 'center'}}>
-                    <Text>text!</Text>
-                </View>
-                <View></View>
+export function UserPage() {
+    const user = useContext(UserContext);
+
+    return (
+        <View style={{flex: 1, justifyContent: 'space-between', backgroundColor: 'white'}}>
+            <PageHeader centerComp={<Text>User</Text>} />
+            <Image source={{
+                uri: user.photoUrl
+            }} style={{width:200, height:200, alignSelf:"center"}}/>
+            <View style={{alignItems: 'center'}}>
+                <Text>text!</Text>
             </View>
-        );
-    }
-  }
+            <View></View>
+        </View>
+    );
+}
