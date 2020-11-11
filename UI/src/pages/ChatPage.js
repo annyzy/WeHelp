d5bcd8980a89997fdcd340d449cdefe03eb5dc2e
Jiajian Lin <jaxlin@g.ucla.edu>
2020-11-10 22:43:58 -0800

    UI: add list view for MessagePage

---
+++ UI/src/pages/ChatPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { PageHeader } from '../components/PageHeader';
+import { ChatBox } from '../components/ChatBox'
+
+export function ChatPage(props) {
+    const {contactUser} = props.route.params;
+
+    return (
+        <View style={{flex: 1, justifyContent: 'flex-start', backgroundColor: 'white'}}>
+            <PageHeader 
+                leftComp={<Button title='Back' onPress={() => props.navigation.goBack()} />}
+                centerComp={<Text>{contactUser.name}</Text>} />
+            <View style={{flex:1}}>
+                <View style={{flex:2, borderWidth:2, justifyContent:"center", alignItems:"center"}}>
+                    <Text>Map!</Text>
+                </View>
+                <View style={{flex:8}}>
+                    <ChatBox contactUser={contactUser}/>
+                </View>
+            </View>
+        </View>
+    );
+}
