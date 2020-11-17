7b8d2b013ee570de1cd1ef408d6debe7f3b152b6
Jiajian Lin <jaxlin@g.ucla.edu>
2020-11-16 17:57:52 -0800

    UI: refactor HomePage

--- UI/src/pages/HomePage.js
+++ UI/src/pages/HomePage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { View, Text, StatusBar, Image, ScrollView } from 'react-native';
-import { SearchBar, Card, Button } from 'react-native-elements';
+import { View, Text, StatusBar, Image, ScrollView, StyleSheet } from 'react-native';
+import { SearchBar, Card } from 'react-native-elements';
 import Constants from 'expo-constants';
 
 export function HomePage() {
-    var keyword;
     const users = [
         {
             name: 'Dongyao',
@@ -19,10 +18,7 @@ export function HomePage() {
             name: 'Wing',
             avatar: 'https://avatars3.githubusercontent.com/u/22208368?s=400&u=3d3f94c135f0c3b6de1601bce6b24c48ee735a44&v=4',
             description: 'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello',
-            img: ['https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4']
+            img: ['https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4']
          },
          {
             name: 'Dongyao',
@@ -37,17 +33,13 @@ export function HomePage() {
             name: 'Wing',
             avatar: 'https://avatars3.githubusercontent.com/u/22208368?s=400&u=3d3f94c135f0c3b6de1601bce6b24c48ee735a44&v=4',
             description: 'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello',
-            img: ['https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4']
+            img: ['https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4']
          },
          {
             name: 'Dongyao',
             avatar: 'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
             description: 'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello',
             img: ['https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
                 'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
                 'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4']
         },
@@ -56,75 +48,138 @@ export function HomePage() {
             avatar: 'https://avatars3.githubusercontent.com/u/22208368?s=400&u=3d3f94c135f0c3b6de1601bce6b24c48ee735a44&v=4',
             description: 'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello',
             img: ['https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
-                'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4',
                 'https://avatars1.githubusercontent.com/u/23393819?s=400&u=7a3a81849ae6c9ef83bb35c31d6826224f8b6528&v=4']
          },
        ]
     return (
         <View style={{flex: 1, justifyContent: 'space-between', backgroundColor:'white'}}>
             <StatusBar barStyle='dark-content'/>
-            <View style={{flex:1, flexDirection:'row', justifyContent:'flex-start', alignItems:'center', top:Constants.statusBarHeight}}>
-                <Image source={require('../../assets/icon.png')} style={{flex:1, top:'1%', height:'100%'}} resizeMode='cover'/>
-                <SearchBar
-                    inputContainerStyle={{height:'65%'}}
-                    containerStyle={{flex:6, backgroundColor:'white'}}
-                    round={true}
-                    platform='ios'
-                    clearButtonMode='always'
-                    showCancel={true}
-                    cancelButtonTitle='cancel'
-                    onChangeText = {keyword}
-                />
-            </View>
+            <SearchField/>
             <View style={{flex: 8, top:Constants.statusBarHeight}}>
                 <ScrollView>
-                    {
-                        users.map((u, i) => {
-                            return (
-                                <Card containerStyle={{marginHorizontal: 5, marginVertical:5, padding:5}} key={i}>
-                                    <View style={{height: 280}}>
-                                        <View style={{flexDirection:'row', flex: 9}}>
-                                            <View style={{flexDirection:'column', justifyContent:'flex-start', flex:1, alignItems:'center'}}>
-                                                <Image source={{uri: u.avatar}} style={{borderWidth:2, flex:1.2, borderRadius:15, width: '85%'}} resizeMode='cover'/>
-                                                <View style={{flex: 4}}>
-                                                    <Text style={{fontSize:15, textAlign:'center'}}>{u.name}</Text>
-                                                    <Text style={{fontSize:15, textAlign:'center'}}>Coin = 0</Text>
-                                                    <Text style={{fontSize:15, textAlign:'center'}}>rating = 0</Text>
-                                                </View>
-                                            </View>
-
-                                            <View style={{flexDirection:'column', justifyContent:'space-between', flex:4}}>
-                                                <View style={{flex:1}}>
-                                                <Text style={{fontSize: 20, textAlign:'center'}}>Title: my name is {u.name}</Text>
-                                                </View>
-                                                <View style={{flex:8, padding:10, justifyContent:'space-between'}}>
-                                                    <Text style={{flex: 4}}>Text Text Text Text Text Text Text Text Text Text Text</Text>
-                                                    <View style={{flexDirection:'row',  justifyContent:'flex-start', flex: 2}}>
-                                                        <Image source={{uri: u.img[0]}} style={{width:'25%'}} resizeMode='contain'/>
-                                                        <Image source={{uri: u.img[1]}} style={{width:'25%'}} resizeMode='contain'/>
-                                                        <Image source={{uri: u.img[2]}} style={{width:'25%'}} resizeMode='contain'/>
-                                                        <Image source={{uri: u.img[3]}} style={{width:'25%'}} resizeMode='contain'/>
-                                                    </View>
-                                                </View>
-                                                <View style={{flexDirection:'row', justifyContent:'space-between',flex: 1}}>
-                                                    <Text style={{fontSize: 20}}>0 likes</Text>
-                                                    <View style={{flexDirection:'row', justifyContent:'flex-end',flex: 9}}>
-                                                        <Text style={{fontSize:20}}>❤️</Text>
-                                                        <Text style={{fontSize:20}}>💬</Text>
-                                                        <Text style={{fontSize:20}}>✅</Text>
-                                                    </View>
-                                                </View>
-                                            </View>
-
-                                        </View>
-                                    </View>
-                                </Card>
-                            )
-                        })
-                    }
+                    {users.map((u, i) => {
+                        return(<CardField user={u} key={i}/>)
+                    })}
                 </ScrollView>
             </View>
         </View>
     );
 }
+
+function SearchField() {
+    return(
+        <View style={{flex:1, flexDirection:'row', justifyContent:'flex-start', alignItems:'center', top:Constants.statusBarHeight}}>
+            <Image source={require('../../assets/icon.png')} style={{flex:1, top:'1%', height:'100%'}} resizeMode='cover'/>
+            <SearchBar
+                inputContainerStyle={{height:'65%'}}
+                containerStyle={{flex:6, backgroundColor:'white'}}
+                round={true}
+                platform='ios'
+                clearButtonMode='always'
+                showCancel={true}
+                cancelButtonTitle='cancel'
+            />
+        </View>
+    );
+}
+
+function CardField(props) {
+    return (
+        <Card containerStyle={styles.cardContainer}>
+            <View style={styles.cardContentView}>
+                <View style={styles.userInfoView}>
+                    <Image source={{ uri: props.user.avatar }} style={styles.avatarStyle} resizeMode='cover' />
+
+                    {/* Need to chagne */}
+                    <View style={{ flex: 4 }}>
+                        <Text style={{ fontSize: 15, textAlign: 'center' }}>{props.user.name}</Text>
+                        <Text style={{ fontSize: 15, textAlign: 'center' }}>Coin = 0</Text>
+                        <Text style={{ fontSize: 15, textAlign: 'center' }}>rating = 0</Text>
+                    </View>
+                    {/* Need to chagne */}
+
+                </View>
+
+                <View style={styles.taskInfoView}>
+                    <Text style={styles.taskTitleStyle}>Title: my name is {props.user.name}</Text>
+                    <View style={styles.taskDescriptionView}>
+                        <Text style={styles.taskTextBoxStyle}>Text Text Text Text Text Text Text Text Text Text Text</Text>
+                        <View style={styles.imageView}>
+                            {props.user.img.map((image, i) => {
+                                return (<Image source={{ uri: image }} style={styles.imageStyle} resizeMode='contain' key={i} />);
+                            })}
+                        </View>
+                    </View>
+                    <View style={styles.actionView}>
+
+                        {/* Need to chagne */}
+                        <Text style={{ fontSize: 20 }}>0 likes</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', flex: 9 }}>
+                            <Text style={{ fontSize: 20 }}>❤️</Text>
+                            <Text style={{ fontSize: 20 }}>💬</Text>
+                            <Text style={{ fontSize: 20 }}>✅</Text>
+                        </View>
+                        {/* Need to chagne */}
+
+                    </View>
+                </View>
+            </View>
+        </Card>
+    );
+}
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        marginHorizontal: 5,
+        marginVertical:5,
+        padding:5
+    },
+    cardContentView: {
+        flexDirection: 'row',
+        flex: 9,
+        height:280
+    },
+    userInfoView: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        flex: 1,
+        alignItems: 'center'
+    },
+    avatarStyle: {
+        borderWidth: 2,
+        borderRadius: 15,
+        width: 60,
+        height: 60
+    },
+    taskInfoView: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        flex: 4
+    },
+    taskTitleStyle: {
+        flex: 1,
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    taskDescriptionView: {
+        flex: 8,
+        padding: 10,
+        justifyContent: 'space-between' 
+    },
+    taskTextBoxStyle: {
+        flex: 4
+    },
+    imageView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        flex: 2
+    },
+    imageStyle: {
+        width: '25%'
+    },
+    actionView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flex: 1
+    }
+})
