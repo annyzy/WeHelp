660357c7cd41074482421b4c6a156769cded7e48
terranzmczmxz <terranzmczmx@gmail.com>
2020-11-29 16:44:51 -0800

    Beautify interface and add refresh functionality on UserPage.js

--- UI/src/pages/UserPage.js
+++ UI/src/pages/UserPage.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useContext, useState } from 'react';
-import { View, ScrollView, Text, Image, Platform, TouchableOpacity, Button } from 'react-native';
+import { View, ScrollView, Text, Image, Platform, TouchableOpacity, Button, RefreshControl, StyleSheet } from 'react-native';
 import { Rating } from 'react-native-elements';
 import { PageHeader } from '../components/PageHeader';
 import { UserContext} from '../components/UserContext';
@@ -8,23 +8,49 @@ import * as expoImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
 import CalendarHeatmap from 'react-native-calendar-heatmap';
 
+const wait = (timeout) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+}
+
+const styles = StyleSheet.create({
+  border: {
+    borderBottomColor: '#D3D3D3',
+    borderBottomWidth: 12,    
+  }, 
+})
+
 export function UserPage(props) {
     const user = useContext(UserContext)[0];
+
+    const [refreshing, setRefreshing] = React.useState(false);
+
+    const onRefresh = React.useCallback(() => {
+      setRefreshing(true);
+
+      wait(2000).then(() => setRefreshing(false));
+    }, []);
 
     return (
         <View style={{flex: 1, justifyContent: 'flex-start', backgroundColor: 'white'}}>
             <PageHeader 
               leftComp={<Button title='Back' onPress={() => props.navigation.goBack()} />}
-              centerComp={<Text>User</Text>} 
+              centerComp={<Text style={{fontSize: 18, textAlign: "center", fontWeight: "bold"}}>Me</Text>} 
               rightComp={<Button title={"Sign Out"}
                           onPress={() => {EventRegister.emit('signOutUser')}}/>}
             />
-            <ScrollView contentContainerStyle={{paddingBottom:Constants.statusBarHeight}}>
-              <View style={{borderBottomWidth: 0.5, flexDirection:'row', justifyContent:'space-around'}}>
-                <View>
+            <ScrollView 
+              contentContainerStyle={{paddingBottom:Constants.statusBarHeight, top:10}}
+              refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+              }
+            >
+              <View style={{borderBottomWidth: 1, borderBottomColor: '#D3D3D3', flexDirection: "column", justifyContent:'space-around'}}>
                 <ImagePicker user={user}/>
-                <Text style={{fontSize:25, alignSelf: 'center'}}>{user.name}</Text>
-                </View>
+                  <Text style={{fontSize:25, alignSelf: 'center'}}>{user.name}</Text>
+              </View>
+              <View style={{borderBottomWidth: 12, borderBottomColor: '#D3D3D3', flexDirection:'row', justifyContent:'space-around'}}>
                 <View style={{alignSelf:'flex-end', padding:5}}>
                   <Rating
                     type='heart'
@@ -34,11 +60,15 @@ export function UserPage(props) {
                     showRating
                     showReadOnlyText={false}
                   />
+                </View>
+                <View style={{justifyContent: "center"}}>
                   <Text style={{ fontSize: 20, textAlign: 'center'}}>👏🏻 5</Text>
                 </View>
               </View>
-              <View style={{top:20}}>
-                <Text style={{fontSize:30, padding: 10, borderBottomWidth: 0.5}}>Contributions</Text>
+              <View style={{top:5, borderBottomWidth: 1, borderBottomColor: '#D3D3D3'}}>
+                <Text style={{fontSize:25, padding: 10}}>Contributions</Text>
+              </View>
+              <View style={{top:10, borderBottomWidth: 12, borderBottomColor: '#D3D3D3'}}>
                 <CalendarHeatmap
                   endDate={new Date()}
                   numDays={120}
@@ -50,10 +80,13 @@ export function UserPage(props) {
                     { date: '2020-11-25' },
                   ]}
                 />
+              </View>
+              <View style={{top:10, borderBottomWidth: 12, borderBottomColor: '#D3D3D3'}}>
+                <Text style={{fontSize:25, padding: 10, borderBottomWidth: 0.5}}>Current Tasks</Text>
+              </View>
+              <View style={{top:10, borderBottomWidth: 12, borderBottomColor: '#D3D3D3'}}>
+                <Text style={{fontSize:25, padding: 10, borderBottomWidth: 0.5}}>Past Tasks</Text>
 
-                <Text style={{fontSize:30, padding: 10, borderBottomWidth: 0.5}}>Current Tasks</Text>
-                
-                <Text style={{fontSize:30, padding: 10, borderBottomWidth: 0.5}}>Past Tasks</Text>
               </View>
             </ScrollView>
 
